eec6aefa3e2e8512b1139cbd2d6143ba53fef4ed
Thomas Chatzidimitris <tchatz@icloud.com>
2018-04-15 11:22:05 +0300

    added process change status

--- public/index.php
+++ public/index.php
@@ -12,9 +12,9 @@ $response = null;
 
 try {
 
-  ini_set('display_errors', 0);
-  ini_set('display_startup_errors', 0);
-  error_reporting(E_ERROR | E_PARSE);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
     error_reporting(E_ALL);
     define("ROOT_DIR", __DIR__ . '/..');
