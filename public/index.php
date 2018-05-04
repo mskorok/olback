5791476b34d4920e847348c644166b6f8f80995b
Thomas Chatzidimitris <tchatz@icloud.com>
2018-05-05 01:50:41 +0300

    process permissions

--- public/index.php
+++ public/index.php
@@ -12,9 +12,9 @@ $response = null;
 
 try {
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+//    ini_set('display_errors', 1);
+//    ini_set('display_startup_errors', 1);
+//    error_reporting(E_ALL);
 //
 //    error_reporting(E_ALL);
     define("ROOT_DIR", __DIR__ . '/..');
