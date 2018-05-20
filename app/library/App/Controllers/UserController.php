54b35dc7c8cb420a52caa987023e7304ebb43009
Thomas Chatzidimitris <tchatz@icloud.com>
2018-05-20 03:00:13 +0300

    d

--- app/library/App/Controllers/UserController.php
+++ app/library/App/Controllers/UserController.php
@@ -788,7 +788,7 @@ $organization_id = $data->organization;
         }
 
         $creator = $this->getUserDetails($creatorId);
-        var_dump($creator);die();
+//        var_dump($creator);die();
         if ($creator['organization'] == null) {
             $response = [
                 'code' => 0,
