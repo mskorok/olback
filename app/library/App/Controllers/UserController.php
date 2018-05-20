61b41c1d3a9996435772c05e9d4d56509c797a03
Thomas Chatzidimitris <tchatz@icloud.com>
2018-05-20 03:02:25 +0300

    d

--- app/library/App/Controllers/UserController.php
+++ app/library/App/Controllers/UserController.php
@@ -802,7 +802,7 @@ $organization_id = $data->organization;
         $organization_id = $creator['organization']->organization_id;
 
         $connection = $this->db;
-        $sql_dist = 'SELECT U.* FROM user U INNER JOIN user_organizationHide O ON O.user_id = U.id WHERE O.organization_id = '.$organization_id.' AND U.email != "deleted@deleted.com" ';
+        $sql_dist = 'SELECT U.* FROM user U INNER JOIN user_organization O ON O.user_id = U.id WHERE O.organization_id = '.$organization_id.' AND U.email != "deleted@deleted.com" ';
         $data_dist = $connection->query($sql_dist);
         $data_dist->setFetchMode(\Phalcon\Db::FETCH_ASSOC);
         $results_dist = $data_dist->fetchAll();
